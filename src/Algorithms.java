b045ea3d352743a70c68a96086237475968ecdde
linusostlund <linusost@kth.se>
2022-01-27 12:11:56 +0100

    add isPrime

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -3,4 +3,16 @@ public class Algorithms{
 	public static void main(String[] args){
 	System.out.println("Hello CI!");	
 	}
+
+	public static boolean isPrime(int num) {
+		if (num <= 1) {
+			return false;
+		}
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+ }
 }
